9cd3033c9b6df1ac896441712ece94c864fb525b
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2023-12-21 19:32:07 +0200

    Add MyDate class

---
+++ Airplane/Airplane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Airplane
+{
+    protected string StartCity;
+    protected string FinishCity;
+    protected MyDate StartDate;
+    protected MyDate FinishDate;
+
+    public int GetTotalTime()
+    {
+        int totalTimeInMinutes = 0;
+
+        return totalTimeInMinutes;
+    }
+    public bool IsArravingToday()
+    {
+        bool isArravingToday = false;
+
+        return isArravingToday;
+    }
+}
---
+++ Airplane/MyDate.cs
@@ -0,0 +1,85 @@
+﻿using System;
+public class MyDate
+{
+    protected short Year;
+    protected byte Month;
+    protected byte Day;
+    protected byte Hours;
+    protected byte Minutes;
+
+    public MyDate()
+    {
+        Year = 2000;
+        Month = 12;
+        Day = 31;
+        Hours = 24;
+        Minutes = 60;
+    }
+    public MyDate(short year, byte month, byte day, byte hours, byte minutes)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+        Hours = hours;
+        Minutes = minutes;
+    }
+    public MyDate(short year, byte month, byte day)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+    public MyDate(byte hours, byte minutes)
+    {
+        Hours = hours;
+        Minutes = minutes;
+    }
+    public MyDate(MyDate myDate)
+    {
+        Year = myDate.Year;
+        Month = myDate.Month;
+        Day = myDate.Day;
+        Hours = myDate.Hours;
+        Minutes = myDate.Minutes;
+    }
+    public void SetMyDateYear(short value)
+    {
+        Year = value;
+    }
+    public void SetMyDateMonth(byte value)
+    {
+        Month = value;
+    }
+    public void SetMyDateDay(byte value)
+    {
+        Day = value;
+    }
+    public void SetMyDateHours(byte value)
+    {
+        Hours = value;
+    }
+    public void SetMyDateMinutes(byte value)
+    {
+        Minutes = value;
+    }
+    public short GetMyDateYear()  
+    { 
+        return Year;
+    }
+    public byte GetMyDateMonth() 
+    { 
+        return Month;
+    }
+    public byte GetMyDateDay() 
+    {  
+        return Day;
+    }
+    public byte GetMyDateHours() 
+    { 
+        return Hours;
+    }
+    public byte GetMyDateMinutes() 
+    { 
+        return Minutes;
+    }
+}
