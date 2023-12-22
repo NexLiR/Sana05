6654428142d579150e005264bb238f21440c008f
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2023-12-22 19:57:12 +0200

    Add Product and Currency classes and constructos and Set-/Get- methods for them

---
+++ Product/Currency.cs
@@ -0,0 +1,48 @@
+﻿using System;
+public class Currency
+{
+    protected string Name;
+    protected double ExRate;
+
+    public Currency()
+    {
+        Name = "Unknown";
+        ExRate = 0;
+    }
+    public Currency(string name, double exRate)
+    {
+        Name = name;
+        ExRate = exRate;
+    }
+    public Currency(string name)
+    {
+        Name = name;
+    }
+    public Currency(double exRate)
+    {
+        ExRate = exRate;
+    }
+    public Currency(Currency currency)
+    {
+        Name = currency.Name;
+        ExRate = currency.ExRate;
+    }
+    public string GetName()
+    {
+        return Name;
+    }
+    public double GetExRate()
+    {
+        return ExRate;
+    }
+    public void SetName(string name)
+    {
+        if (name.Length > 0) Name = name;
+        else throw new Exception("Name can not be empty");
+    }
+    public void SetExRate(double exRate)
+    {
+        if (exRate > 0) ExRate = exRate;
+        else throw new Exception("Exchange rate can not be negative");
+    }
+}
---
+++ Product/Product.cs
@@ -0,0 +1,103 @@
+﻿using System;
+public class Product
+{
+    protected string Name;
+    protected float Price;
+    protected Currency Cost;
+    protected int Quantity;
+    protected string Producer;
+    protected float Weight;
+
+    public Product()
+    {
+        Name = "Unknown";
+        Price = 0;
+        Cost = new Currency();
+        Quantity = 0;
+        Producer = "Unknown";
+        Weight = 0;
+    }
+    public Product(string name, float price, Currency cost, int quantity, string producer, float weight)
+    {
+        Name = name;
+        Price = price;
+        Cost = cost;
+        Quantity = quantity;
+        Producer = producer;
+        Weight = weight;
+    }
+    public Product(string name, float price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+    public Product(string producer, float weight)
+    {
+        Producer = producer;
+        Weight = weight;
+    }
+    public Product(Product product)
+    {
+        Name = product.Name;
+        Price = product.Price;
+        Cost = product.Cost;
+        Quantity = product.Quantity;
+        Producer = product.Producer;
+        Weight = product.Weight;
+    }
+    public string GetName()
+    {
+        return Name;
+    }
+    public float GetPrice()
+    {
+        return Price;
+    }
+    public Currency GetCost()
+    {
+        return Cost;
+    }
+    public int GetQuantity()
+    {
+        return Quantity;
+    }
+    public string GetProducer()
+    {
+        return Producer;
+    }
+    public float GetWeight()
+    {
+        return Weight;
+    }
+    public void SetName(string name)
+    {
+        if (name.Length > 0) Name = name;
+        else throw new Exception("Name can not be empty");
+    }
+    public void SetPrice(float price)
+    {
+        if (price > 0) Price = price;
+        else throw new Exception("Price can not be negative");
+    }
+    public void SetCost(Currency cost)
+    {
+        Cost = cost;
+    }
+    public void SetQuantity(int quantity)
+    {
+        if (quantity >= 0) Quantity = quantity;
+        else throw new Exception("Quantity can not be negative");
+    }
+    public void SetProducer(string producer)
+    {
+        if (producer.Length > 0) Producer = producer;
+        else throw new Exception("Producer can not be empty");
+    }
+    public void SetWeight(float weight)
+    {
+        if (weight > 0) Weight = weight;
+        else throw new Exception("Weight can not be negative");
+    }
+
+}
